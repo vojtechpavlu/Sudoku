7f8375655fc4bef26658fbeb38bfc387814dd5fc
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2022-12-28 21:01:42 +0100

    Add `GridGenerator` class
    Defined class responsible for random grid generation. This class is abstract to define a mutual protocol for each of the descendants inheriting from it. At the same time, it defines a common methods to enhance the usability and reduce code redundancy.

--- src/grid_generation.py
+++ src/grid_generation.py
@@ -2,6 +2,58 @@
 
 from .grid import Grid, Field
 from random import shuffle, choice
+from abc import ABC, abstractmethod
+
+
+class GridGenerator(ABC):
+    """"""
+
+    def __init__(self, num_of_empty_values: int):
+        """"""
+        assert (
+            0 <= num_of_empty_values < 81,
+            f"Počet prázndých políček musí být mezi [0, 81): "
+            f"({num_of_empty_values})")
+        self.__empty_values = num_of_empty_values
+
+    @property
+    def num_of_empty_values(self) -> int:
+        """"""
+        return self.__empty_values
+
+    @property
+    def empty_grid(self) -> Grid:
+        """"""
+        # Pomocný kontejner pro prázdná políčka
+        empty_fields = []
+
+        # Pro všechny souřadnice os `x` a `y` vygeneruj prázdná políčka, tedy
+        # políčka s hodnotou 0
+        for x in range(9):
+            for y in range(9):
+                empty_fields.append(Field(x, y, 0))
+
+        # Vrať grid plný prázdných hodnot
+        return Grid(empty_fields)
+
+    def empty_values(self, grid: Grid) -> Grid:
+        """"""
+        for _ in range(self.num_of_empty_values):
+            choice(grid.filled_fields).value = 0
+        return grid
+
+    @abstractmethod
+    def generate(self) -> Grid:
+        """"""
+
+    @staticmethod
+    def possible_values(grid: Grid, x: int, y: int) -> list[int]:
+        """"""
+        possibles = []
+        for potential_value in range(1, 10):
+            if grid.can_be_at(potential_value, x, y):
+                possibles.append(potential_value)
+        return possibles
 
 
 def _possible_values(grid: Grid, x: int, y: int):
