eb6c954d60261e04b65008bc9142e1920ae746b1
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2022-12-28 21:25:02 +0100

    Add `BacktrackingGenerator` class
    Defined class of `BacktrackingGenerator`, which is responsible for "Depth-First Search"-like generation of a random grid.

--- src/grid_generation.py
+++ src/grid_generation.py
@@ -99,55 +99,35 @@ class GridGenerator(ABC):
         return possibles
 
 
-def _possible_values(grid: Grid, x: int, y: int):
-    possibles = []
-    for i in range(1, 10):
-        if grid.can_be_at(i, x, y):
-            possibles.append(i)
-    return tuple(possibles)
-
-
-def random_grid_generator() -> Grid:
+class BacktrackingGenerator(GridGenerator):
     """"""
-    empty_fields = []
 
-    for i in range(9):
-        for j in range(9):
-            empty_fields.append(Field(i, j, 0))
+    def generate(self) -> Grid:
+        """"""
+        generated_grid = None
 
-    generated_grid = None
+        try:
+            self.__fill_grid(self.empty_grid)
+        except _GridGenerated as generated:
+            generated_grid = generated.grid
 
-    try:
-        _fill_grid(Grid(empty_fields))
-    except _GridGenerated as generated:
-        generated_grid = generated.grid
+        return generated_grid
 
-    return generated_grid
+    def __fill_grid(self, grid: Grid) -> Grid:
+        """"""
+        for x in range(9):
+            for y in range(9):
+                field = grid.field(x, y)
+                if field.is_empty:
+                    possibles = self.possible_values(grid, field.x, field.y)
+                    shuffle(possibles)
+                    for value in possibles:
+                        field.value = value
+                        self.__fill_grid(grid)
+                        field.value = 0
+                    return grid
 
-
-def values_emptier(grid: Grid, empties: int) -> Grid:
-    """"""
-    assert 81 > empties > 0, "Počet prázdných musí být v intervalu [1; 80]"
-    for _ in range(empties):
-        choice(grid.filled_fields).value = 0
-    return grid
-
-
-def _fill_grid(grid: Grid) -> Grid:
-    """"""
-    for x in range(9):
-        for y in range(9):
-            field = grid.field(x, y)
-            if field.is_empty:
-                possibles = list(_possible_values(grid, field.x, field.y))
-                shuffle(possibles)
-                for value in possibles:
-                    field.value = value
-                    _fill_grid(grid)
-                    field.value = 0
-                return grid
-
-    raise _GridGenerated(grid)
+        raise _GridGenerated(grid)
 
 
 class _GridGenerated(Exception):
