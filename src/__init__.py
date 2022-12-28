736c76181ae1b9bef4092fba5a5dd284e5f92ae6
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2022-12-28 21:49:48 +0100

    Package-level import shorthand

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""Základní zdrojový balíček obsahující prostředky pro generování úplných
+a konzistentních konfigurací hrací plochy sudoku.
+"""
+
+from src.grid import Field, Grid
+from src.grid_generation import GridGenerator, BacktrackingGenerator
